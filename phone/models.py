cd9307c2975714187f6d8ea1882c5add2fddd410
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-11-29 17:03:37 +0300

    Update models.py

--- phone/models.py
+++ phone/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from slugify import slugify  # Подключаем библиотеку для слагификации
+from slugify import slugify 
 
 class Phone(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.URLField()
