374fc511ef9573c0b9cbfeebf09bc99b1fee0b72
Алиса <128479231+NeMaliavka@users.noreply.github.com>
2024-11-29 17:04:32 +0300

    Update import_phones.py

--- phone/management/commands/import_phones.py
+++ phone/management/commands/import_phones.py
@@ -12,18 +12,25 @@ class Command(BaseCommand):
         path = os.path.abspath('phones.csv')
         print(os.path.exists(path))  # Проверка существования файла
 
-        with open(path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=';')
-            for row in reader:
-                slug = slugify(row['name'])  # Генерация slug
-                phone = Phone(
-                    id=row['id'], 
-                    name=row['name'],
-                    price=row['price'],
-                    image=row['image'],
-                    release_date=row['release_date'],
-                    lte_exists=row['lte_exists'] == 'True',  # Преобразование строки в булевый тип
-                    slug=slug
-                )
-                print(phone)
-                phone.save()  # Сохранение в базу данных
+        try:
+            with open(path, newline='') as csvfile:
+                reader = csv.DictReader(csvfile, delimiter=';')
+                for row in reader:
+                    slug = slugify(row['name'])  # Генерация slug
+                    # Проверка на дубликаты
+                    if Phone.objects.filter(id=row['id']).exists():
+                        print(f"Phone with id {row['id']} already exists. Skipping.")
+                        continue
+                    
+                    phone = Phone(
+                        name=row['name'],
+                        price=row['price'],
+                        image=row['image'],
+                        release_date=row['release_date'],
+                        lte_exists=row['lte_exists'] == 'True',  # Преобразование строки в булевый тип
+                        slug=slug
+                    )
+                    print(phone)
+                    phone.save()  # Сохранение в базу данных
+        except Exception as e:
+            print(f"Error reading CSV file: {e}")
